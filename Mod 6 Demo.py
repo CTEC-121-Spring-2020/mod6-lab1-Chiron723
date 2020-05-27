a66f62966eac2d88bb2b433e9dafef6381df8a08
Stephen Guild <s.guild2@students.clark.edu>
2020-05-27 12:21:53 -0700

    Mod 6 lab 1

---
+++ Mod 6 Demo.py
@@ -0,0 +1,48 @@
+"""
+CTEC 121
+<your name>
+<assignment/lab name>
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    '''
+    for i in range(0,11,2):
+        print(i)
+    print()
+
+    count=0
+    while count <=11:
+        print(count)
+        #count=count+2
+        count+=2
+    print()
+
+    x=True
+    y=True
+    if (x):
+        if(y):
+            print("both true")
+        else:
+            print("y false")
+    else:
+        print("x false")
+
+    if (x and y):
+        print("both true")
+    else:
+        print("other")
+        '''
+
+    print(True and False)
+    print(True and True)
+    print(True and False or False)
+    print(False or False or True)
+
+main()    
